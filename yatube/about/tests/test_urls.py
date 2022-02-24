e4b923818ab14ee317bab11673f70e88e5524c2f
Сергей Чувычкин <chuvichkin@yandex.ru>
2022-02-24 23:14:05 +0300

    5 спринт_тесты1

--- yatube/about/tests/test_urls.py
+++ yatube/about/tests/test_urls.py
@@ -18,5 +18,5 @@ class StaticURLTests(TestCase):
         self.assertEqual(response.status_code, 200)
 
     def test_about_tech_url_uses_correct_template(self):
-        response = self.guest_client.get('/page/about/tech/')
+        response = self.guest_client.get('/about/tech/')
         self.assertTemplateUsed(response, 'about/tech.html')
