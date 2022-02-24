cfd31f59053ea5eb0e1efb2c6e9c4813f7326513
Сергей Чувычкин <chuvichkin@yandex.ru>
2022-02-24 23:09:27 +0300

    5 спринт_тесты1

--- yatube/about/tests/test_urls.py
+++ yatube/about/tests/test_urls.py
@@ -6,21 +6,17 @@ class StaticURLTests(TestCase):
         self.guest_client = Client()
 
     def test_about_author_url_exists_at_desired_location(self):
-        """Проверка доступности адреса /page/about/."""
-        response = self.guest_client.get('/page/about/author/')
+        response = self.guest_client.get('/about/author/')
         self.assertEqual(response.status_code, 200)
 
     def test_about_author_url_uses_correct_template(self):
-        """Проверка шаблона для адреса /page/about/."""
-        response = self.guest_client.get('/page/about/author/')
+        response = self.guest_client.get('/about/author/')
         self.assertTemplateUsed(response, 'about/author.html')
 
     def test_about_tech_url_exists_at_desired_location(self):
-        """Проверка доступности адреса /page/about/."""
-        response = self.guest_client.get('/page/about/')
+        response = self.guest_client.get('/about/tech/')
         self.assertEqual(response.status_code, 200)
 
     def test_about_tech_url_uses_correct_template(self):
-        """Проверка шаблона для адреса /page/about/."""
         response = self.guest_client.get('/page/about/tech/')
         self.assertTemplateUsed(response, 'about/tech.html')
